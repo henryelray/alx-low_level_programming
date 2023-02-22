1d6cb0fe7ac84ca6f86591ad3135edc0e99e70fa
henryelray <sehenrysolomon@outlook.com>
2023-02-21 18:13:55 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * main - Entry point of program
- * Description: prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  * Return: Always (0 == Success)
 */
-int main(void)
+void print_alphabet_x10(void)
 {
 	int i;
 
@@ -18,5 +17,4 @@ int main(void)
 		}
 		_putchar('\n');
 	}
-	return (0);
 }
