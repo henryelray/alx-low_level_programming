318b00f4e36e7502045d7734c7abba1134a1d55f
henryelray <sehenrysolomon@outlook.com>
2023-02-23 15:56:18 -0800

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - causes an infinite loop
  * Return: 0
 **/
+
 int main(void)
 {
 	int i;
 
+	rintf("Infinite loop incoming :(\n");
+
 	i = 0;
-	positive_or_negative(i);
+
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
