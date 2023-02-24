a0bc6a257f507d7a0e0c4e0e8049673cabcfe1e6
henryelray <sehenrysolomon@outlook.com>
2023-02-23 18:51:27 -0800

    11.prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	long prime_num = 612852475143, div;
+
+	while (div < (prime_num / 2))
+	{
+		if (prime_num % 2 == 0)
+		{
+
+			prime_num /= 2;
+			continue;
+		}
+		for ( div = 3; div < (prime_num / 2); div += 2)
+		{
+			if ((prime_num % div) == 0)
+				prime_num /= div;
+		}
+	}
+	printf("%ld\n", prime_num);
+	return (0);
+}	
